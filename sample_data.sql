a2c232b149d19dce6a891a8fc278dbedf3cb5eb5
vdevigere <vdevigere+git@gmail.com>
2023-11-30 18:04:12 -0800

    Updated sample_data.sql that showcases all out of the box activations.

--- sample_data.sql
+++ sample_data.sql
@@ -5,55 +5,17 @@ DELETE FROM flags;
 
 DELETE FROM activations;
 
--- Seed the flags table
-INSERT INTO flags
-            (name,
-             status)
-VALUES     ('Date Activated Feature',
-            TRUE);
+INSERT INTO activations VALUES(1,'Date Activated','novi.client.activations.date_time_activation.DateTimeActivation','{"startDateTime":"11/26/2023 12:00 AM","endDateTime":"11/28/2023 12:00 AM","format": "%m/%d/%Y %I:%M %p"}');
+INSERT INTO activations VALUES(2,'Random Split Activated','novi.client.activations.weighted_random_activation.WeightedRandomActivation','{ "splits":[100, 0, 0], "variations":["A", "B", "C"]}');
+INSERT INTO activations VALUES(3,'Combo AND Activation','novi.client.activations.and_activation.AndActivation','[1,2]');
+INSERT INTO activations VALUES(4,'Combo OR Activation','novi.client.activations.or_activation.OrActivation','[1,2]');
 
-INSERT INTO flags
-            (name,
-             status)
-VALUES     ('Random Variant Feature',
-            TRUE);
+INSERT INTO flags VALUES(1,'Date Activated Feature',1);
+INSERT INTO flags VALUES(2,'Random Variant Feature',1);
+INSERT INTO flags VALUES(3,'Combo AND',1);
+INSERT INTO flags VALUES(4,'Combo OR',1);
 
-
--- Seed the activations table
-INSERT INTO activations
-            (name,
-			class_name,
-             config)
-VALUES     ("Date Activated",
-			"novi_activations.standard.date_time_activation.DateTimeActivation",
-            '{"startDateTime":"11/26/2023 12:00 AM","endDateTime":"11/28/2023 12:00 AM","format": "%m/%d/%Y %I:%M %p"}'
-            );
-
-INSERT INTO activations
-            (name,
-			class_name,
-             config)
-VALUES     ("Random Split Activated",
-			"novi_activations.standard.weighted_random_activation.WeightedRandomActivation",
-            '{ "splits":[100, 0, 0], "variations":["A", "B", "C"]}');
-
--- Seed the flags_activations table
-INSERT INTO flags_activations
-            (flag_id,
-             activation_id)
-SELECT flags.id,
-       activations.id
-FROM   flags,
-       activations
-WHERE  flags.name == 'Date Activated Feature'
-       AND activations.name = 'Date Activated';
-
-INSERT INTO flags_activations
-            (flag_id,
-             activation_id)
-SELECT flags.id,
-       activations.id
-FROM   flags,
-       activations
-WHERE  flags.name == 'Random Variant Feature'
-       AND activations.name = 'Random Split Activated';
+INSERT INTO flags_activations VALUES(1,1);
+INSERT INTO flags_activations VALUES(2,2);
+INSERT INTO flags_activations VALUES(3,3);
+INSERT INTO flags_activations VALUES(4,4);
